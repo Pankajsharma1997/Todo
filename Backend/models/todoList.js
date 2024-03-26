2a732b30caf6bec8f931a53bc3b37e2c32eb5e67
Pankaj sharam <Pankajsharma1997>
2024-03-26 18:18:40 +0530

    add new field in model

--- Backend/models/todoList.js
+++ Backend/models/todoList.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 const todoSchema = new mongoose.Schema
     ({
+        name :{
+            type: String,
+            required:true
+        },
         task: {
             type:String,
             required:true
+        },
+        status:{
+            type:String,
+            required:true
+        },
+        date:{
+            type:Date
         },
         completed:{
             type: Boolean,
